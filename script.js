01e180b84dc9f358171cf55cc2fd0f7729d3dbd1
Harvey909090 <92372679+Harvey909090@users.noreply.github.com>
2021-12-02 15:13:43 +0300

    removing console message used for testing

--- script.js
+++ script.js
@@ -39,11 +39,9 @@ function generateAkan() {
   
    // formula computation
   var d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-  console.log(d);
   
   //removing any decimal value
   var d = parseInt(d);
-   console.log(d);
   
    if (!isNaN(d)) {
       if (gender == "female") {
