bea7e30777ae249de565b1c469fa57d7b5e72857
Harvey909090 <92372679+Harvey909090@users.noreply.github.com>
2021-12-02 15:03:23 +0300

    function to be called on clicking generate button

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const birth_month = document.getElementById("mob");
 const birth_date = document.getElementById("dob");
 const birth_year = document.getElementById("yob");
 const user_gender = document.getElementById("gender")
+
+function generateAkan() {
+  var day = parseInt(birth_date.value);
+  var month = parseInt(birth_month.value);
+  var year = parseInt(birth_year.value);
+  var gender = user_gender.value;
+}
