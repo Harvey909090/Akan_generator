48326707e6988f1b767a60846da9ae42e1ec83a9
Harvey909090 <92372679+Harvey909090@users.noreply.github.com>
2021-12-02 15:01:32 +0300

    variable declaration of inputs

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+//variable declaration of inputs
+const birth_month = document.getElementById("mob");
+const birth_date = document.getElementById("dob");
+const birth_year = document.getElementById("yob");
+const user_gender = document.getElementById("gender")
